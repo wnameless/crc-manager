08f0bbdacb9a112dbff7e1b2fb9876572d962d48
wmw <wnameless@gmail.com>
2018-01-16 14:34:58 +0800

    Redirect index to cases

--- src/main/java/com/wmw/crc/manager/controller/IndexController.java
+++ src/main/java/com/wmw/crc/manager/controller/IndexController.java
@@ -17,18 +17,13 @@
  */
 package com.wmw.crc.manager.controller;
 
-import static com.google.common.base.Charsets.UTF_8;
-
 import java.io.IOException;
-import java.net.URL;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import com.google.common.io.Resources;
 import com.wmw.crc.manager.form.json.schema.FormJsonSchemaProvider;
 
 @Controller
@@ -38,26 +33,9 @@ public class IndexController {
   FormJsonSchemaProvider schemaProvider;
 
   @RequestMapping(path = { "/", "index" })
-  String index(@RequestParam(value = "name", required = false,
-      defaultValue = "Sam") String name, Model model) throws IOException {
-    model.addAttribute("name", name);
+  String index(Model model) throws IOException {
 
-    model.addAttribute("formSchema", schemaProvider.getFormSchema());
-    model.addAttribute("formUiSchema", schemaProvider.getFormUiSchema());
-    model.addAttribute("formSupplement1Schema",
-        schemaProvider.getFormSupplement1Schema());
-    model.addAttribute("formSupplement1UiSchema",
-        schemaProvider.getFormSupplement1UiSchema());
-    model.addAttribute("formSupplement2Schema",
-        schemaProvider.getFormSupplement2Schema());
-    model.addAttribute("formSupplement2UiSchema",
-        schemaProvider.getFormSupplement2UiSchema());
-
-    URL url = Resources.getResource("json-schema/form-data.json");
-    String formData = Resources.toString(url, UTF_8);
-    model.addAttribute("formData", formData);
-
-    return "index";
+    return "redirect:/cases";
   }
 
 }
