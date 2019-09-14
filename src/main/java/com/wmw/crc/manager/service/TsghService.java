5c56b44e3c882695d271a1dbc289a113a06ff727
wmw <wnameless@gmail.com>
2019-09-14 22:03:44 +0800

    Upgrade spring boot

--- src/main/java/com/wmw/crc/manager/service/TsghService.java
+++ src/main/java/com/wmw/crc/manager/service/TsghService.java
@@ -16,184 +16,29 @@
 package com.wmw.crc.manager.service;
 
 import java.io.IOException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
-
 import com.github.wnameless.advancedoptional.AdvOpt;
-import com.wmw.crc.manager.model.CaseStudy;
-import com.wmw.crc.manager.model.CaseStudy.Status;
-import com.wmw.crc.manager.model.Contraindication;
-import com.wmw.crc.manager.model.Medicine;
-import com.wmw.crc.manager.model.Subject;
-import com.wmw.crc.manager.repository.CaseStudyRepository;
-import com.wmw.crc.manager.repository.MedicineRepository;
 import com.wmw.crc.manager.service.tsgh.api.Drug;
 import com.wmw.crc.manager.service.tsgh.api.Patient;
 import com.wmw.crc.manager.service.tsgh.api.PatientContraindication;
-import com.wmw.crc.manager.service.tsgh.api.SimpleDrug;
-import com.wmw.crc.manager.service.tsgh.api.TsghApi;
-import com.wmw.crc.manager.service.tsgh.api.TsghResponse;
 
 import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
-import net.sf.rubycollect4j.Ruby;
-import net.sf.rubycollect4j.RubyArray;
-import net.sf.rubycollect4j.RubyHash;
-import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
-import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Call;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
-@Slf4j
-@Service
-public class TsghService {
+public interface TsghService {
 
-  @Value("${api.tsgh.baseurl}")
-  String baseUrl;
+  public Patient findPatientById(String nationalId) throws IOException;
 
-  TsghApi tsghApi;
-
-  @Autowired
-  CaseStudyRepository caseRepo;
-
-  @Autowired
-  MedicineRepository medicineRepo;
-
-  @PostConstruct
-  void postConstruct() {
-    HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-    interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-    OkHttpClient client =
-        new OkHttpClient.Builder().addInterceptor(interceptor).build();
-
-    Retrofit retrofit = new Retrofit.Builder().baseUrl(baseUrl).client(client)
-        .addConverterFactory(GsonConverterFactory.create()).build();
-    tsghApi = retrofit.create(TsghApi.class);
-  }
-
-  public Patient findPatientById(String nationalId) throws IOException {
-    Call<TsghResponse<Patient>> call = tsghApi.searchPatient(nationalId);
-    Response<TsghResponse<Patient>> res = call.execute();
-    TsghResponse<Patient> body = res.body();
-    return body == null ? null : body.getData();
-  }
-
-  public List<Drug> getDrugs() throws IOException {
-    Call<TsghResponse<List<Drug>>> call = tsghApi.listDrugs();
-    Response<TsghResponse<List<Drug>>> res = call.execute();
-    return res.body().getData();
-  }
+  public List<Drug> getDrugs() throws IOException;
 
   public Response<ResponseBody> addPatientContraindication(
-      PatientContraindication pc) throws IOException {
-    Call<ResponseBody> call = tsghApi.addPatientContraindication(pc);
-    Response<ResponseBody> res = call.execute();
-    return res;
-  }
+      PatientContraindication pc) throws IOException;
 
-  public AdvOpt<Integer> refreshMedicines() {
-    List<Drug> drugs;
-    try {
-      drugs = getDrugs();
-      if (!drugs.isEmpty()) medicineRepo.deleteAll();
-    } catch (IOException e) {
-      log.error("TsghService#getDrugs failed.", e);
-      return AdvOpt.ofNullable(null, "TsghService::getDrugs failed.");
-    }
+  public AdvOpt<Integer> refreshMedicines();
 
-    for (Drug drug : drugs) {
-      Medicine med = new Medicine();
-      med.setName(drug.getName());
-      med.setEngName(drug.getEngName());
-      med.setScientificName(drug.getScientificName());
-      med.setHospitalCode(drug.getHospitalCode());
-      if (drug.getAtcCode() != null) {
-        RubyArray<String> atcCodes = Ruby.Array.of(drug.getAtcCode());
-        med.setAtcCode1(atcCodes.at(0));
-        med.setAtcCode2(atcCodes.at(1));
-        med.setAtcCode3(atcCodes.at(2));
-        med.setAtcCode4(atcCodes.at(3));
-      }
-      med.setTakekind(drug.getTakekind());
-      medicineRepo.save(med);
-    }
-
-    return AdvOpt.of(drugs.size(),
-        "Total " + drugs.size() + " medicines has been updated.");
-  }
-
-  public AdvOpt<ContraindicationRefreshResult> refreshContraindications() {
-    ContraindicationRefreshResult crr = new ContraindicationRefreshResult();
-
-    List<CaseStudy> cases = caseRepo.findByStatus(Status.EXEC);
-    for (CaseStudy c : cases) {
-      RubyHash<Integer, RubyArray<Contraindication>> bundles = Ruby.Array
-          .of(c.getContraindications()).groupBy(Contraindication::getBundle);
-
-      for (Subject s : c.getSubjects()) {
-        PatientContraindication pc = new PatientContraindication();
-        pc.setNationalId(s.getNationalId());
-        pc.setIrbName(c.getTrialName());
-        pc.setIrbNumber(c.getIrbNumber());
-        pc.setPatientId(s.getPatientId());
-        pc.setStartDate(LocalDate.parse(c.getExpectedStartDate())
-            .format(DateTimeFormatter.ofPattern("yyyyMMdd")));
-        pc.setEndDate(LocalDate.parse(c.getExpectedEndDate())
-            .format(DateTimeFormatter.ofPattern("yyyyMMdd")));
-
-        if (bundles.containsKey(s.getContraindicationBundle())) {
-          RubyArray<Contraindication> cds =
-              bundles.get(s.getContraindicationBundle());
-          for (Contraindication cd : cds) {
-            List<Medicine> meds = medicineRepo
-                .findByNameContainsOrEngNameContainsOrScientificNameContainsAllIgnoreCase(
-                    cd.getPhrase(), cd.getPhrase(), cd.getPhrase());
-
-            for (Medicine med : meds) {
-              if (cd.getTakekinds().contains(med.getTakekind())) {
-                SimpleDrug sd = new SimpleDrug();
-                sd.setPhrase(cd.getPhrase());
-                sd.setAtcCode(med.getAtcCode1());
-                sd.setHospitalCode(med.getHospitalCode());
-                sd.setMemo(cd.getMemo());
-                pc.getDrugs().add(sd);
-              }
-            }
-          }
-        }
-
-        try {
-          Response<ResponseBody> res = addPatientContraindication(pc);
-
-          if (res.isSuccessful()) {
-            crr.increaseSuccessCount();
-          } else {
-            log.error("TsghService#addPatientContraindication for patient("
-                + s.getNationalId() + ") failed.");
-            crr.increaseFailedCount();
-          }
-        } catch (IOException e) {
-          log.error("TsghService#addPatientContraindication for patient("
-              + s.getNationalId() + ") failed.", e);
-          crr.increaseFailedCount();
-        }
-      }
-    }
-
-    return AdvOpt.of(crr,
-        "" + crr.getSuccessCount() + " contraindications has been added. "
-            + crr.getFailedCount() + " contraindications are failed.");
-  }
+  public AdvOpt<ContraindicationRefreshResult> refreshContraindications();
 
   @Data
   public static final class ContraindicationRefreshResult {
---
+++ src/main/java/com/wmw/crc/manager/service/TsghServiceTestImpl.java
@@ -0,0 +1,146 @@
+/*
+ *
+ * Copyright 2019 Wei-Ming Wu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+package com.wmw.crc.manager.service;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import com.github.wnameless.advancedoptional.AdvOpt;
+import com.wmw.crc.manager.model.CaseStudy;
+import com.wmw.crc.manager.model.CaseStudy.Status;
+import com.wmw.crc.manager.model.Contraindication;
+import com.wmw.crc.manager.model.Medicine;
+import com.wmw.crc.manager.model.Subject;
+import com.wmw.crc.manager.repository.CaseStudyRepository;
+import com.wmw.crc.manager.repository.MedicineRepository;
+import com.wmw.crc.manager.service.tsgh.api.Drug;
+import com.wmw.crc.manager.service.tsgh.api.Patient;
+import com.wmw.crc.manager.service.tsgh.api.PatientContraindication;
+import com.wmw.crc.manager.service.tsgh.api.SimpleDrug;
+
+import lombok.extern.slf4j.Slf4j;
+import net.sf.rubycollect4j.Ruby;
+import net.sf.rubycollect4j.RubyArray;
+import net.sf.rubycollect4j.RubyHash;
+import okhttp3.ResponseBody;
+import retrofit2.Response;
+
+@Profile("mock")
+@Slf4j
+@Service
+public class TsghServiceTestImpl implements TsghService {
+
+  @Autowired
+  CaseStudyRepository caseRepo;
+
+  @Autowired
+  MedicineRepository medicineRepo;
+
+  @Override
+  public Patient findPatientById(String nationalId) throws IOException {
+    return null;
+  }
+
+  @Override
+  public List<Drug> getDrugs() throws IOException {
+    return new ArrayList<>();
+  }
+
+  @Override
+  public Response<ResponseBody> addPatientContraindication(
+      PatientContraindication pc) throws IOException {
+    System.out.println(pc);
+    return null;
+  }
+
+  @Override
+  public AdvOpt<Integer> refreshMedicines() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public AdvOpt<ContraindicationRefreshResult> refreshContraindications() {
+    ContraindicationRefreshResult crr = new ContraindicationRefreshResult();
+
+    List<CaseStudy> cases = caseRepo.findByStatus(Status.EXEC);
+    for (CaseStudy c : cases) {
+      RubyHash<Integer, RubyArray<Contraindication>> bundles = Ruby.Array
+          .of(c.getContraindications()).groupBy(Contraindication::getBundle);
+
+      for (Subject s : c.getSubjects()) {
+        PatientContraindication pc = new PatientContraindication();
+        pc.setNationalId(s.getNationalId());
+        pc.setIrbName(c.getTrialName());
+        pc.setIrbNumber(c.getIrbNumber());
+        pc.setPatientId(s.getPatientId());
+        pc.setStartDate(LocalDate.parse(c.getExpectedStartDate())
+            .format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+        pc.setEndDate(LocalDate.parse(c.getExpectedEndDate())
+            .format(DateTimeFormatter.ofPattern("yyyyMMdd")));
+
+        if (bundles.containsKey(s.getContraindicationBundle())) {
+          RubyArray<Contraindication> cds =
+              bundles.get(s.getContraindicationBundle());
+          for (Contraindication cd : cds) {
+            List<Medicine> meds = medicineRepo
+                .findByNameContainsOrEngNameContainsOrScientificNameContainsAllIgnoreCase(
+                    cd.getPhrase(), cd.getPhrase(), cd.getPhrase());
+
+            for (Medicine med : meds) {
+              if (cd.getTakekinds().contains(med.getTakekind())) {
+                SimpleDrug sd = new SimpleDrug();
+                sd.setPhrase(cd.getPhrase());
+                sd.setAtcCode(med.getAtcCode1());
+                sd.setHospitalCode(med.getHospitalCode());
+                sd.setMemo(cd.getMemo());
+                pc.getDrugs().add(sd);
+              }
+            }
+          }
+        }
+
+        try {
+          Response<ResponseBody> res = addPatientContraindication(pc);
+
+          if (res != null && res.isSuccessful()) {
+            crr.increaseSuccessCount();
+          } else {
+            log.error("TsghService#addPatientContraindication for patient("
+                + s.getNationalId() + ") failed.");
+            crr.increaseFailedCount();
+          }
+        } catch (IOException e) {
+          log.error("TsghService#addPatientContraindication for patient("
+              + s.getNationalId() + ") failed.", e);
+          crr.increaseFailedCount();
+        }
+      }
+    }
+
+    return AdvOpt.of(crr,
+        "" + crr.getSuccessCount() + " contraindications has been added. "
+            + crr.getFailedCount() + " contraindications are failed.");
+  }
+
+}
