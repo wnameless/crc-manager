6e34e82016841faa9cd17c170b43fb3ce4420a9c
wmw <wnameless@gmail.com>
2020-05-26 16:03:20 +0800

    Improve js script tags

--- src/main/java/com/wmw/crc/manager/controller/SubjectController.java
+++ src/main/java/com/wmw/crc/manager/controller/SubjectController.java
@@ -248,9 +248,8 @@ public class SubjectController implements NestedRestfulController< //
 
   @PreAuthorize("@perm.canWrite(#parentId)")
   @GetMapping("/query/{nationalId}")
-  @ResponseBody
-  Subject searchPatient(@PathVariable Long parentId,
-      @PathVariable String nationalId) {
+  String searchPatient(Model model, @PathVariable Long parentId,
+      @PathVariable String nationalId, Locale locale) {
     Subject subject;
     try {
       subject = tsghService.queryPatientById(nationalId);
@@ -259,7 +258,12 @@ public class SubjectController implements NestedRestfulController< //
       subject = new Subject();
     }
 
-    return subject;
+    updateChild(model, subject);
+    if (subject.getNationalId() == null) {
+      model.addAttribute("message",
+          i18n.msg("ui.subject.message.id-no-data", locale));
+    }
+    return "subjects/new :: partial";
   }
 
   @PreAuthorize("@perm.canWrite(#parentId)")
